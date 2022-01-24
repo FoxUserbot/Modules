ac0dd60f32addeb5977ae08ad3b3bb8eaaa4ca57
A9FM <68541852+A9FM@users.noreply.github.com>
2022-01-25 00:14:53 +0200

    Add files via upload

---
+++ kickall_iris.py
@@ -0,0 +1,24 @@
+import asyncio
+from pyrogram import Client, filters
+from plugins.settings.main_settings import module_list, file_list
+
+from prefix import my_prefix
+
+prefix = my_prefix()
+
+
+@Client.on_message(filters.command("kickall iris", prefixes=prefix) & filters.me)
+async def tagall(client, message):
+    await message.delete()
+    chat_id = message.chat.id
+    string = ""
+    limit = 1
+    icm = client.iter_chat_members(chat_id)
+    async for member in icm:
+        string = f"бан {member.user.mention}\n"
+        await client.send_message(chat_id, text=string)
+        await asyncio.sleep(2)
+
+
+module_list['Tagall'] = f'{prefix}tagall'
+file_list['Tagall'] = 'tagall.py'
