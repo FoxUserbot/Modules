bb3c037611a87dfaa38cfc51b49e274e48f27160
A9FM <68541852+A9FM@users.noreply.github.com>
2022-02-22 00:15:59 +0300

    RU \ EN v | by @Phonk_Basss

--- covid.py
+++ covid.py
@@ -6,7 +6,7 @@ from prefix import my_prefix
 prefix = my_prefix()
 
 
-@Client.on_message(filters.command("covid", prefixes=prefix) & filters.me)
+@Client.on_message(filters.command("covid_en", prefixes=prefix) & filters.me)
 async def covid_local(client, message):
     region = " ".join(message.command[1:])
     await message.edit("<code>Data retrieval...</code>")
@@ -30,16 +30,29 @@ async def covid_local(client, message):
         await message.edit(f'<code>There is no region called "{region}"</code>')
 
 
-@Client.on_message(filters.command("regions", prefixes=prefix) & filters.me)
-async def regions(client, message):
-    countr = ""
-    await message.edit("<code>Data retrieval...</code>")
+@Client.on_message(filters.command("covid_ru", prefixes=prefix) & filters.me)
+async def covid_local(client, message):
+    region = " ".join(message.command[1:])
+    await message.edit("<code>Получение данных...</code>")
     covid = Covid(source="worldometers")
-    regions = covid.list_countries()
-    for region in regions:
-        region = f"{region}\n"
-        countr += region
-    await message.edit(f"<code>{countr}</code>")
+    try:
+        local_status = covid.get_status_by_country_name(region)
+        await message.edit(
+            "<b>=====🦠 Статистика COVID-19 🦠=====</b>\n"
+            + f"<b>Страна</b>: <code>{local_status['country']}</code>\n"
+            + "<b>==================================</b>\n"
+            + f"<b>🤧 Новые заражения</b>: <code>{local_status['new_cases']}</code>\n"
+            + f"<b>😷 Новые смерти</b>: <code>{local_status['new_deaths']}</code>\n"
+            + "<b>==================================</b>\n"
+            + f"<b>😷 Подтвержденные</b>: <code>{local_status['confirmed']}</code>\n"
+            + f"<b>❗️ Болеют:</b> <code>{local_status['active']}</code>\n"
+            + f"<b>⚠️ В тяжелом состоянии</b>: <code>{local_status['critical']}</code>\n"
+            + f"<b>💀 Всего смертей</b>: <code>{local_status['deaths']}</code>\n"
+            + f"<b>🚑 Всего выздоровели</b>: <code>{local_status['recovered']}</code>\n"
+        )
+    except ValueError:
+        await message.edit(f'<code>Нет такой страны с названием  "{region}"</code>')
 
-module_list['StatisticsCovid19'] = f'{prefix}covid [region]'
+
+module_list['StatisticsCovid19'] = f'{prefix}covid_[en|ru] [region]'
 file_list['StatisticsCovid19'] = 'covid.py'
